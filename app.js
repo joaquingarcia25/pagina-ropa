0f12e0fb6e409f319d495ca8147046eacde7e40f
Joaquin <joaquingarcia25@yahoo.com.ar>
2022-08-11 21:24:17 -0300

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,31 @@
+//variables
+
+let nombre = "joaquin" //string
+console.log(nombre)
+let edad = 18 //number
+console.log(edad)
+let estudiante = true; //boolean
+console.log(estudiante)
+const apellido = "garcia"
+console.log(apellido)
+    //dif let const//
+    //puede ser inicializadas vacias//
+    //pueden ser redeclaradas//
+    //las variables ponerlas en MAY es buena practica//
+let nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto)
+
+let numero1 = 35
+let numero2 = -20
+
+let resultado = numero1 + " " + numero2
+console.log(resultado)
+
+let primernombre = prompt ("ingrese su nombre")
+alert ("!Hola Joaquin")
+
+let numero4 = Number(prompt("ingresa el primer number"))
+let numero5 = Number(prompt("ingresa el segundo number"))
+
+let resultado3 = numero4 + numero5 
+alert(resultado3)
