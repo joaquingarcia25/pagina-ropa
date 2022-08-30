8800543253b058d206f54d5dac5a2a8d8e7e977a
joaquin <joaquingarcia25@gmail.com>
2022-08-30 09:34:43 -0300

    funciones y html

--- app.js
+++
@@ -1,6 +0,0 @@
-let number = (prompt("ingresa un numero"));
-
-for (let i = 2;i <= 15; i++){
-let resultado = number * i;
-alert(`${number} x ${i} = ${resultado}`)
-}
---
+++ jv/app.js
@@ -0,0 +1,14 @@
+let primeranota = 0;
+let segundanota = 0;
+
+function notas (primeranota, segundanota, operacion) {
+        switch (operacion) {
+          case "+":
+            return resultado = primeranota + segundanota;
+            break;
+          case "/":
+            return resultado / 2;
+            break;
+        }} 
+        console.log (notas(7, 8, "+"))
+        console.log(notas(7, 8,  "/"))
